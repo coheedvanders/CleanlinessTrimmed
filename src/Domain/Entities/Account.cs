86552987ea64b08bd43f20a301c6fb24528f23c5
Miguel Ramos <coheedramos@gmail.com>
2025-03-06 20:37:54 +0800

    created database and account table

---
+++ src/Domain/Entities/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanlinessTrimmed.Domain.Entities
+{
+    public class Account: BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
--- src/Infrastructure/Data/ApplicationDbContext.cs
+++ src/Infrastructure/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using CleanlinessTrimmed.Application.Common.Interfaces;
+using CleanlinessTrimmed.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace CleanlinessTrimmed.Infrastructure.Data;
@@ -7,6 +8,8 @@ namespace CleanlinessTrimmed.Infrastructure.Data;
 public class ApplicationDbContext : DbContext, IApplicationDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<Account> Accounts => Set<Account>();
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
